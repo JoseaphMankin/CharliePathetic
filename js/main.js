b44276f5d80f0afc73297d08419e4f7615388de0
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-05-30 21:59:39 -0500

    Sidenav Work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-	$('.parallax').parallax();
-	$('.materialboxed').materialbox();
-	$('.button-collapse').sideNav();
+// $(document).ready(function() {
+// 	$('.parallax').parallax();
+// 	$('.materialboxed').materialbox();
+// 	$('.button-collapse').sideNav();
+// });
+
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems, { edge: 'right' });
 });
