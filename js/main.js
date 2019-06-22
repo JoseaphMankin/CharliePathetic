6d21c8c1875ba906549daa09d999942e9f06fa90
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-06-21 22:50:40 -0500

    Button rework

--- js/main.js
+++ js/main.js
@@ -14,3 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.modal');
 	var instances = M.Modal.init(elems, options);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.tooltipped');
+	var instances = M.Tooltip.init(elems, options);
+});
