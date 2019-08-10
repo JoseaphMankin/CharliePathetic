df3fb22ecd6b283f25fc8b4fae374961aa4149af
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-08-10 09:18:39 -0500

    youtube fixes

--- js/main.js
+++ js/main.js
@@ -7,7 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.carousel');
-	var instances = M.Carousel.init(elems, options);
+	var instances = M.Carousel.init(elems, {
+		numVisible: 5,
+		indicators: true,
+	});
 });
 
 document.addEventListener('DOMContentLoaded', function() {
